8334403af9f9e58ea82e2cdd9bb5285aec6dda56
Yerko Aguirre <yerko.aguirre@gmail.com>
2014-11-18 23:46:21 -0300

    Added spring security and metrics.

--- src/main/java/com/awards/web/rest/VoteResource.java
+++ src/main/java/com/awards/web/rest/VoteResource.java
@@ -1,12 +1,12 @@
 package com.awards.web.rest;
 
 import com.awards.domain.Vote;
-import com.awards.service.UserService;
+import com.awards.security.AuthoritiesConstants;
 import com.awards.service.VoteService;
+import com.codahale.metrics.annotation.Timed;
 import com.wordnik.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,19 +14,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+
 @RestController
 @RequestMapping("/app")
-@Api(value = "Votes", description = "Cast, and updates votes")
 public class VoteResource {
     private final Logger log = LoggerFactory.getLogger(VoteResource.class);
 
-    @Autowired
+    @Inject
     private VoteService voteService;
 
-    @ApiOperation(value = "Cast one vote")
-    @ApiResponses( {@ApiResponse( code = 200, message = "Should always return Http Status OK" )} )
     @RequestMapping(value = "/rest/vote", method = RequestMethod.POST)
-    public ResponseEntity<?> castVote(@ApiParam(name = "Vote", value = "Vote to be casted", required = true)
+    @Timed
+    @RolesAllowed(AuthoritiesConstants.ADMIN)
+    public ResponseEntity<String> castVote(@ApiParam(name = "Vote", value = "Vote to be casted", required = true)
                                             @RequestBody(required = true) Vote vote) {
         log.info("Casting one vote for user {}, category {}, nominee {}", vote.getUserId()
                 ,vote.getCategoryId()
